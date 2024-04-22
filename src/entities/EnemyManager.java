632b16d545090310c9a9b489b93fbad871519bce
Dinh18 <152268268+Dinh18@users.noreply.github.com>
2024-04-23 04:38:03 +0700

    Update EnemyManager.java
    doi ten contants

--- src/entities/EnemyManager.java
+++ src/entities/EnemyManager.java
@@ -77,8 +77,8 @@ public class EnemyManager {
 		for (Crabby c : currentLevel.getCrabs())
 			if (c.isActive()) {
 
-				g.drawImage(crabbyArr[c.getState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset - CRABBY_DRAWOFFSET_X + c.flipX(),
-						(int) c.getHitbox().y - CRABBY_DRAWOFFSET_Y + (int) c.getPushDrawOffset(), CRABBY_WIDTH * c.flipW(), CRABBY_HEIGHT, null);
+				g.drawImage(crabbyArr[c.getState()][c.getAniIndex()], (int) c.getHitbox().x - xLvlOffset - PIG_DRAWOFFSET_X + c.flipX(),
+						(int) c.getHitbox().y - PIG_DRAWOFFSET_Y + (int) c.getPushDrawOffset(), PIG_WIDTH * c.flipW(), PIG_HEIGHT, null);
 
 //				c.drawHitbox(g, xLvlOffset);
 //				c.drawAttackBox(g, xLvlOffset);
@@ -119,7 +119,7 @@ public class EnemyManager {
 	}
 
 	private void loadEnemyImgs() {
-		crabbyArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE), 9, 5, CRABBY_WIDTH_DEFAULT, CRABBY_HEIGHT_DEFAULT);
+		crabbyArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.CRABBY_SPRITE), 9, 5, PIG_WIDTH_DEFAULT, PIG_HEIGHT_DEFAULT);
 		pinkstarArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.PINKSTAR_ATLAS), 8, 5, PINKSTAR_WIDTH_DEFAULT, PINKSTAR_HEIGHT_DEFAULT);
 		sharkArr = getImgArr(LoadSave.GetSpriteAtlas(LoadSave.SHARK_ATLAS), 8, 5, SHARK_WIDTH_DEFAULT, SHARK_HEIGHT_DEFAULT);
 	}
