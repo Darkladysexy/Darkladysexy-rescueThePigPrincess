352e58dbb08acb19759667dbe6fbaa7c809f9751
Linh <ngtualinh2oo4@gmail.com>
2024-04-23 14:58:39 +0700

    Update LoadSave.java

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -11,19 +11,19 @@ import javax.imageio.ImageIO;
 
 public class LoadSave {
 
-	public static final String PLAYER_ATLAS = "player_sprites.png";
-	public static final String LEVEL_ATLAS = "outside_sprites.png";
+	public static final String PLAYER_ATLAS = "combined_image (5).png";
+	public static final String LEVEL_ATLAS = "orange_brick.png";
 	public static final String MENU_BUTTONS = "button_atlas.png";
 	public static final String MENU_BACKGROUND = "menu_background.png";
 	public static final String PAUSE_BACKGROUND = "pause_menu.png";
 	public static final String SOUND_BUTTONS = "sound_button.png";
 	public static final String URM_BUTTONS = "urm_buttons.png";
 	public static final String VOLUME_BUTTONS = "volume_buttons.png";
-	public static final String MENU_BACKGROUND_IMG = "background_menu.png";
+	public static final String MENU_BACKGROUND_IMG = "Dự án mới (7).png";
 	public static final String PLAYING_BG_IMG = "playing_bg_img.png";
 	public static final String BIG_CLOUDS = "big_clouds.png";
 	public static final String SMALL_CLOUDS = "small_clouds.png";
-	public static final String CRABBY_SPRITE = "crabby_sprite.png";
+	public static final String PIG_SPRITE = "Pig.png";
 	public static final String STATUS_BAR = "health_power_bar.png";
 	public static final String COMPLETED_IMG = "completed_sprite.png";
 	public static final String POTION_ATLAS = "potions_sprites.png";
